2c588cb9e80bb5b214e4f87e94019f6f4733ae10
gouf <innocent.zero@gmail.com>
2017-10-17 19:57:04 +0900

    Update step to success
    puts method execution no needs failure detection

--- lib/blog_post/operation/create.rb
+++ lib/blog_post/operation/create.rb
@@ -2,18 +2,16 @@ require 'trailblazer/operation'
 
 module BlogPost
   class Create < Trailblazer::Operation
-    # Step has a flow that executes up to down
-    step :hello_world!
-    step :how_are_you?
+    # success method ignores return value
+    success :hello_world!
+    success :how_are_you?
 
     def hello_world!(options, *)
       puts 'Hello, Trailblazer!'
-      # true
     end
 
     def how_are_you?(options, *)
       puts 'How are you?'
-      true
     end
   end
 end
--- spec/blog_post/operation/create_spec.rb
+++ spec/blog_post/operation/create_spec.rb
@@ -3,8 +3,6 @@ describe BlogPost::Create do
     # it always returns Trailblazer::Operation::Result object
     result = BlogPost::Create.() # same as .call()
 
-    # executed step is only hello_world! method.
-    # subsequent steps not be executed.
-    expect(result.failure?).to be_truthy # => OK
+    expect(result.success?).to be_truthy # => OK
   end
 end
