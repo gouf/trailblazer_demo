8127e0795d75b6aa39a7e2cbb07bcc45b03269b6
gouf <innocent.zero@gmail.com>
2017-10-17 18:14:13 +0900

    RSpec: add test target requrie

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require_relative '../lib/dummy_class'
+require_relative '../lib/blog_post/operation/create'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
