574e03c315d0e66e0fdc73616ae4ef9b25739c09
gouf <innocent.zero@gmail.com>
2017-10-17 20:31:17 +0900

    RSpec: veryfy that a joke told

--- spec/blog_post/operation/create_spec.rb
+++ spec/blog_post/operation/create_spec.rb
@@ -1,21 +1,13 @@
 describe BlogPost::Create do
   it 'operation test' do
     # it always returns Trailblazer::Operation::Result object
-    result = BlogPost::Create.({happy: 'yes'}) # receiving data from out side
-    # =>
-    # Hello, Trailblazer!
-    # How are you?
-    # Good to hear, have a nice day!
-
-    expect(result.success?).to be_truthy # => OK
-  end
-
-  it 'operation test2 (failure ver.)' do
-    result = BlogPost::Create.({happy: 'no'})
+    result = BlogPost::Create.({happy: false}) # receiving data from out side
     # =>
     # Hello, Trailblazer!
     # How are you?
 
-    expect(result.failure?).to be_truthy
+    expect(result.failure?).to be_truthy # => OK
+
+    expect(result['joke']).to eq 'Broken pencils are pointless.' # => OK
   end
 end
